7630aad19fb2584aba1e5b9a03585653f802d89a
Lukas Kilian <lukas@kilian.ltd>
2019-01-26 15:51:52 +0100

    status on all monitors

--- config.h
+++ config.h
@@ -4,12 +4,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gap pixel between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 10;       /* gap pixel between windows */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=11" };
-static const char dmenufont[]       = "IBM Plex Mono:size=14";
+static const char *fonts[]          = { "IBM Plex Sans:size=12" };
 
 static const char col_bg[]          = "#222222";
 static const char col_fg_sel[]      = "#bbbbbb";
