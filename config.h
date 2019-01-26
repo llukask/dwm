cd511c6641ae7c9fe794328b3e6f0b0f89bfb0f1
Lukas Kilian <lukas@kilian.ltd>
2019-01-26 13:49:14 +0100

    center wname

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=14" };
+static const char *fonts[]          = { "Iosevka:size=11" };
 static const char dmenufont[]       = "IBM Plex Mono:size=14";
 
 static const char col_gray1[]       = "#222222";
