8d8461bafcbea179d32c46cbf16331c82a5b639a
Lukas Kilian <lukas@kilian.ltd>
2019-01-26 12:46:57 +0100

    colors

--- config.h
+++ config.h
@@ -8,15 +8,21 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka:size=14" };
 static const char dmenufont[]       = "IBM Plex Mono:size=14";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+static const char col_bg[]          = "#222222";
+static const char col_fg_sel[]      = "#bbbbbb";
+static const char col_fg_norm[]     = "#888888";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg_norm, col_bg, col_bg },
+	[SchemeSel]  = { col_fg_sel, col_bg, col_bg  },
 };
 
 /* tagging */
@@ -58,7 +64,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -fn \"Iosevka-16\"", NULL };
-static const char *termcmd[]  = { "st", NULL };
+// static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
